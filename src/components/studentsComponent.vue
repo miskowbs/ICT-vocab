6734a5df39aa024e37e7f56f8a16e9670a3260a2
miskowbs <bmiskowiec@gmail.com>
2018-11-13 18:07:30 +0900

    fleshed out studentsComponent

--- src/components/studentsComponent.vue
+++ src/components/studentsComponent.vue
@@ -1,5 +1,58 @@
 <template>
   <v-container fluid grid-list-lg class="elevation-12 ma-0"> 
-    
+    <h3 class="display-3 mb-auto">All Students</h3>
+    <v-expansion-panel
+      v-model="showStudentDetails"
+      popout>
+      <v-expansion-panel-content
+        v-for="(student, index) in students"
+        :key="index">
+        <v-layout 
+          slot="header"
+          @click="viewStudentsList(index)">
+          <v-flex 
+            xs8
+            class="display-1 mb-auto text-capitalize">
+            {{ student.name }}
+          </v-flex>
+          <!-- TODO: PUT, in another v-flex the last 
+          logged in and last modified dates -->
+        </v-layout>
+        <v-card>
+          <!-- TODO: Put Lists Component here
+          Factor in that if something is added/changed
+          here, that it will not be reflected in the last
+          seen fields -->
+        </v-card>
+      </v-expansion-panel-content>
+    </v-expansion-panel>
   </v-container>
 </template>
+
+<script>
+import { users } from "../firebase";
+var firebase = require('firebase');
+import listsComponent from "./listsComponent";
+
+export default {
+  props: {
+
+  },
+  data() {
+    return {
+    showStudentDetails: [],
+    students: []
+    }
+  },
+  firestore() {
+    return {
+      students: users.where("role", "==", "student")
+    } 
+  },
+  methods: {
+    viewStudentsList(index) {
+
+    }
+  }
+}
+</script>
